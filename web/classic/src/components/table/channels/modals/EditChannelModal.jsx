42bd45646838ad9838c552d7d268b9e0624f39e3
NTRUCK <ntruck@users.noreply.github.com>
2026-07-12 21:01:06 +0800

    Revert "修复单Key渠道追加时提交key_mode"
    This reverts commit 0c2bbfa96d320a2c29bebdea3c8d438f53eedb0d.

--- web/classic/src/components/table/channels/modals/EditChannelModal.jsx
+++ web/classic/src/components/table/channels/modals/EditChannelModal.jsx
@@ -3104,7 +3104,7 @@ const EditChannelModal = (props) => {
                       </>
                     )}
 
-                    {isEdit && inputs.type !== 57 && (
+                    {isEdit && isMultiKeyChannel && (
                       <Form.Select
                         field='key_mode'
                         label={t('密钥更新模式')}
